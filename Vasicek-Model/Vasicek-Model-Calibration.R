b5c285e1fcdc82175a05456ea6546db63d093588
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2019-08-14 22:45:59 +0800

    Update Vasicek-Model-Calibration.R

--- Vasicek-Model/Vasicek-Model-Calibration.R
+++ Vasicek-Model/Vasicek-Model-Calibration.R
@@ -1,33 +1,57 @@
 # DRAFT MODEL
 # The following code demonstrate the clibration of parameters of Vasicek model (One factor short rate model)
-# True parameters in simulation
-dt <- 0.25 # Time steps
-lambda <- 3 # Mean reversion speed
-mu <- 1 # Long term mean level (annualized)
-sigma <- 0.5 # Volatility of interest rate (annualized)
-trial <- 10^5 # Number of Trial
+# HIBOR data from HSB
+setwd('C:\\Users\\user\\Documents\\Programming\\Note on R\\Vasicek Model_Calibration')
+interest.rate.data <- read.csv('hibor.csv')
+head(interest.rate.data, 10) # Show data
+sapply(interest.rate.data, class) # Check data types
 
-r <- rep(0, trial) 
-r[1] <- 3 # Assumption
-for (i in 2:trial) {
-  # Ornstein-Uhlenbeck process SDE
-  dr <- lambda*(mu - r[i - 1])*dt + sigma*sqrt(dt)*rnorm(1)
-  r[i] <- r[i-1] + dr
-}
+interest.rate.data$Date <- as.Date(unlist(interest.rate.data$Date), '%d/%m/%Y')
 
 # Ordinary Least Square (OLS) Estimation
 # Vasicek model SDE can be discretized as an AR(1) Process in finite time
-lm <- lm(r[2:trial] ~ r[1:trial - 1])
-summary(lm)
+row.count <- nrow(interest.rate.data)
+interest.rate.data$HIBOR.lag <- c(NA, interest.rate.data$HIBOR[1:row.count - 1])
 
-a <- lm$coefficients[2]
-b <- lm$coefficients[1]
-res.var <- var(lm$residuals)
+reg.lm <- lm(HIBOR ~ HIBOR.lag, data = interest.rate.data)
+summary(reg.lm) # Show regression output
+
+a <- reg.lm$coefficients[2]
+b <- reg.lm$coefficients[1]
+res.var <- var(reg.lm$residuals)
+dt <- 1/252
 
 # SDE Parameters calibrated by OLS
-calibrated.mu <- b/(1 - a)
-calibrated.lambda <- (1 - a)/dt
-calibrated.sigma <- sqrt(res.var/dt)
-cat('Estimated mu', calibrated.mu, '\n')
-cat('Estimated lambda', calibrated.lambda, '\n')
-cat('Estimated sigma', calibrated.sigma, '\n')
+mu <- b/(1 - a)
+lambda <- (1 - a)/dt
+sigma <- sqrt(res.var/dt)
+
+cat('Estimated mu: ', mu, '\n')
+cat('Estimated lambda: ', lambda, '\n')
+cat('Estimated sigma: ', sigma, '\n')
+
+plot(interest.rate.data$HIBOR,
+     type = 'l', lwd = 2,
+     ylab = 'HIBOR Rate',
+     xlab = 'Date')
+
+# Simulation
+trial <- row.count # Length of paths to be simulated
+sim.count <- 15 # Number of paths to be simulated
+
+for (j in 1:sim.count){
+  r <- rep(0, trial)
+  r[1] <- interest.rate.data$HIBOR[1]
+
+  for (i in 2:trial) {
+    # Ornstein-Uhlenbeck process SDE
+    dr <- lambda*(mu - r[i - 1])*dt + sigma*sqrt(dt)*rnorm(1)
+    r[i] <- r[i-1] + dr
+  }
+  lines(r, type = 'l', col = runif(1, 0, 20))
+}
+
+
+
+
+
